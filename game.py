e897e4dcf6bce186f423d4fcfc2d06f6bbbef9d4
Pavel Chembulatov <mr.chembulatov@yandex.ru>
2024-10-30 14:59:38 +0500

    Minor code readability improvements

--- game.py
+++ game.py
@@ -5,16 +5,20 @@ from gameparts.exceptions import FieldIndexError, CellOccupiedError
 
 def main():
     print('Добро пожаловать в игру "крестики-нолики"!\n')
+    
     game = Board()
     game.display()
+
     current_player = 'X'
 
     running = True
     while running:
         print(f'\nСейчас ходят {current_player}')
+
         row, column = coordinates_input(game)
         game.make_move(row, column, current_player)
         game.display()
+
         running = check_for_game_over(game, current_player)
         current_player = change_player(current_player)
 
@@ -22,6 +26,7 @@ def main():
 def coordinates_input(game):
     while True:
         try:
+
             row = int(input('Введите номер строки: '))
             if row < 1 or row > game.field_size:
                 raise FieldIndexError
@@ -30,54 +35,64 @@ def coordinates_input(game):
                 raise FieldIndexError
             if game.board[row-1][column-1] != ' ':
                 raise CellOccupiedError
+            
         except FieldIndexError:
+
             print(f'Значение должно быть от 1 до {game.field_size}.')
             continue
+
         except ValueError:
+
             print('Буквы вводить нельзя. Только числа.')
             print('Пожалуйста, введите значения заново.')
+
         except CellOccupiedError:
+
             print('Ячейка уже занята!')
             print('Введите другие координаты.')
+
         except Exception as e:
+
             print(f'Возникла ошибка: {e}.')
+
         else:
+
             print('\n')
             return row, column
 
 
 def change_player(current_player):
-    current_player = 'O' if current_player == 'X' else 'X'
-    return current_player
+    return 'O' if current_player == 'X' else 'X'
 
 
-def game_result(result, winner_name=''):
+def save_game_result(result, winner_name=''):
     game_end_time = datetime.now().strftime('%d/%m/%Y %H:%M')
     log_text = f'{game_end_time} {result}{winner_name}\n'
-    save_result(log_text)
+    with open('results.txt', 'a') as result_log:
+        result_log.write(log_text)
 
 
 def check_for_game_over(game, current_player):
     if game.check_win(current_player):
+
         result = f'Победили {current_player}!'
         print(f'\n{result}')
+
         winner_name = f' Победитель: {input('Введите имя победителя: ')}'
-        game_result(result, winner_name)
+        save_game_result(result, winner_name)
+
         return False
 
     if game.is_board_full():
+
         result = 'Ничья!'
         print(f'\n{result}')
-        game_result(result, winner_name='')
+
+        save_game_result(result)
+
         return False
 
     return True
-
-
-def save_result(log_text):
-    result_log = open('results.txt', 'a')
-    result_log.write(log_text)
-    result_log.close()
 
 
 if __name__ == '__main__':
